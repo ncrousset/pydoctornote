67f2b7068d8d0db3d7496057af460a9b3f471411
Rudys Natanael Acosta <natanael926@gmail.com>
2021-09-07 23:19:02 -0400

    Model patients and test

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from accounts.models import CustomUser as User
 
-# Create your models here.
+
+class Patient(models.Model):
+    SEX = (
+        ('m', 'masculine'),
+        ('f', 'feminine'),
+        ('o', 'other'),
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    insurance = models.CharField(max_length=20, blank=True, null=True)
+    idd = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    sex = models.CharField(max_length=15, choices=SEX)
+    next_appointment = models.DateField(blank=True, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    @property
+    def full_name(self):
+        """Returns the patient's full name"""
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Patient
+
+
+class PatientTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@gmail.com',
+            password='test'
+        )
+
+        self.patient = Patient.objects.create(
+            first_name='Natanael',
+            last_name='Acosta',
+            birth_date='2021-05-18',
+            email='natanael926@gmail.com',
+            insurance='454555',
+            idd='545456',
+            phone='5454545',
+            sex='m',
+            next_appointment='2021-05-18',
+            user_id=self.user
+        )
+
+    def test_string_representation(self):
+        patient = Patient(first_name='Natanael', last_name='Acosta')
+        self.assertEqual(str(patient), self.patient.full_name)

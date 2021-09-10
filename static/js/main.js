5485978ecd6df6b3647e00f5e6417dd5b197f0dd
Rudys Natanael Acosta <natanael926@gmail.com>
2021-09-09 22:37:57 -0400

    added style to de link

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,29 @@
+// function toggle for de modal
 function toggleModal(modalID) {
     document.getElementById(modalID).classList.toggle("hidden");
     document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
     document.getElementById(modalID).classList.toggle("flex");
     document.getElementById(modalID + "-backdrop").classList.toggle("flex");
 }
+
+// add style classes for the active link, where the user is browsing
+function decorationLink() {
+
+    let path = location.pathname
+
+    let link = {
+        '/app/patients': 'patients_link'
+    }
+
+    if (path != undefined) {
+        element = document.getElementById(link[path])
+
+        if (element != null) {
+            element.classList.add('font-semibold')
+        }
+    }
+}
+
+(function () {
+    decorationLink()
+})();

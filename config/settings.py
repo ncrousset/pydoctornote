a006d2d286ad18849f12fd7a446aae9a3595173b
Rudys Natanael Acosta <natanael926@gmail.com>
2021-09-10 15:54:53 -0400

    Test heroku

--- config/settings.py
+++ config/settings.py
@@ -85,14 +85,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'POST': os.getenv('DB_POST'),
-    }
+    'default': env.dj_db_url('DATABASE_URL')
+    # 'default': {
+    #     'ENGINE': os.getenv('DB_ENGINE'),
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST': os.getenv('DB_HOST'),
+    #     'POST': os.getenv('DB_POST'),
+    # }
 }
 
 

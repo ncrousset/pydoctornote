a70ec94111abcd6d262de1eb57f20d1302849b17
Rudys Natanael Acosta <natanael926@gmail.com>
2021-09-10 20:51:28 -0400

    Fixed patients create

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,11 @@ from .views import (dashboard, list_patient_view,
 
 urlpatterns = [
     path('', dashboard, name="dashboard"),
-    re_path(r'^patients/$',
-            list_patient_view, name="list_patients"),
+
     path('patient/<int:pk>', patient_view, name="detail_patient"),
     path('patients/create/', PatientCreateView.as_view(), name='create_patient'),
     path('patients/update/<int:pk>',
          PatientUpdateView.as_view(), name='update_patient'),
+    re_path(r'^patients/$',
+            list_patient_view, name="list_patients"),
 ]
